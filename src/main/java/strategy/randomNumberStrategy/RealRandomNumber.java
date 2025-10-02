6b20e28d4fb0dcf9848f40473da09f97894950c8
RedSunSmile <naver-or@hanmail.net>
2025-10-02 15:08:18 +0900

    feat(radomNumberStrategy): 자동차난수범위추가

---
+++ src/main/java/strategy/randomNumberStrategy/RealRandomNumber.java
@@ -0,0 +1,15 @@
+package strategy.randomNumberStrategy;
+
+import java.util.Random;
+
+public class RealRandomNumber implements RandomNumber {
+  private Random random;
+
+  public RealRandomNumber(Random random) {
+    this.random = random;
+  }
+
+  public int generate() {
+    return random.nextInt(10);
+  }
+}
